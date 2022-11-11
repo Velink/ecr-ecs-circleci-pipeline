130e36c38ac45c7c2277922cb105d4e8b87e6264
Velin Kalenderski <velinkalenderski@hotmail.co.uk>
2022-11-11 09:44:52 +0000

    add proper image tagging and fix linter errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080;
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -10,5 +10,5 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`)
   console.log('Hello World')
 
-  process.exit();
+  process.exit()
 })
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 test('1 plus 1 should be equal to 2', () => { 
-  const a = 1;
-  const b = 1;
-  expect(a + b).toBe(2);
-});
+  const a = 1
+  const b = 1
+  expect(a + b).toBe(2)
+})
