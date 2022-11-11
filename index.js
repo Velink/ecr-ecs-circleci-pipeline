980601c511163d16bccc092ea9bf2dfd901458f8
Velin Kalenderski <velinkalenderski@hotmail.co.uk>
2022-11-11 09:06:26 +0000

    linter test

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const app = express()
-
-const port  = 8080;
+const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-    console.log('Hello World')
+  console.log(`Server running on port ${port}`)
+  console.log('Hello World')
 
-    process.exit();
+  process.exit();
 })
--- index.test.js
+++ index.test.js
@@ -1,6 +1,4 @@
-const request = require('supertest');
-
-test("1 plus 1 should be equal to 2", () => { 
+test('1 plus 1 should be equal to 2', () => { 
   const a = 1;
   const b = 1;
   expect(a + b).toBe(2);
